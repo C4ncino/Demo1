d185ee71c7dc8afde417385f9da0bc68fb9da7fb
C4ncino <193305@iberopuebla.mx>
2022-10-11 08:06:04 -0500

    Lista

--- app-demo/src/components/Lista.js
+++ app-demo/src/components/Lista.js
@@ -2,34 +2,24 @@ import React from "react";
 import Alumno from "./Alumno";
 
 //Component
-const Lista = (listNombres, listApellidos, listMatriculas) => {
-    let listAlumnos = []
-    
-    const creaAlumno = () => {
-        listNombres.map( (nombre, i) => {
-            listAlumnos.push( <li> <Alumno name = {nombre} lastname = {listApellidos[i]} mat = {listMatriculas[i]}/> </li>)
-        })
-    }
+const Lista = ({listNombres, listApellidos, listMatriculas}) => {
 
-    creaAlumno();
-
-
+    const listAlumnos = listNombres.map((nombre, i) =>
+        <Alumno name = {nombre} lastname = {listApellidos[i]} mat = {listMatriculas[i]}/>
+    )
 
     return (
       <>
         <h1>Lista de Alumnos</h1>
         <ul>
-            {/* <li>
-                <Alumno name = "Carlos" lastname = "Cancino" mat = "193305"/>
-            </li>
 
-            <li>
-                <Alumno name = "Jp" lastname = "Haro" mat = "193305"/>
-            </li> */}
-            
-            {listAlumnos.map( (alumno, i) => {
-            return <li key = {i}>{alumno}</li>
-            } )}
+            {listAlumnos.map((alumno, i) => {
+                return (
+                    <li key = {i}> 
+                        {alumno}
+                    </li>
+                )
+            })}
 
         </ul>
       </>
